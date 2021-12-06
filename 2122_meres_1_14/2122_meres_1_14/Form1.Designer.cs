1d15e53243e91bf58902c40c9a6312653c91c4f6
M4x0ut <60852001+M4x0ut@users.noreply.github.com>
2021-12-06 09:54:37 +0100

    Add files via upload

---
+++ 2122_meres_1_14/2122_meres_1_14/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace _2122_meres_1_14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.keretossz = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.szamlaszam = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.szamossz = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(13, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(370, 88);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Új pályázat";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(390, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(398, 88);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.keretossz,
+            this.szamlaszam,
+            this.szamossz});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(775, 318);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // keretossz
+            // 
+            this.keretossz.HeaderText = "Keretösszeg";
+            this.keretossz.Name = "keretossz";
+            // 
+            // szamlaszam
+            // 
+            this.szamlaszam.HeaderText = "Számlák száma";
+            this.szamlaszam.Name = "szamlaszam";
+            // 
+            // szamossz
+            // 
+            this.szamossz.HeaderText = "Számlák összege";
+            this.szamossz.Name = "szamossz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn keretossz;
+        private System.Windows.Forms.DataGridViewTextBoxColumn szamlaszam;
+        private System.Windows.Forms.DataGridViewTextBoxColumn szamossz;
+    }
+}
+
---
+++ 2122_meres_1_14/2122_meres_1_14/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _2122_meres_1_14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            string connectionString =
+                @"Server=(localdb)\MSSQLLocalDB;" +
+                "Database=palyazatok;";
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            var sqlCommand = new SqlCommand("SELECT palyazat.id, sum(palyazat.keretossz + palyazat.szamossz)", connection);
+            var sqlDataReader = sqlCommand.ExecuteReader();
+
+            while (sqlDataReader.Read())
+            {
+                
+            }
+
+            sqlDataReader.Close();
+            InitializeComponent();
+        }
+
+        
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
